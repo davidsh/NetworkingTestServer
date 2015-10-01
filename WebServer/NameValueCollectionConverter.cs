eb1484a8aad616085d1939b41fcd772037a86fb5
David Shulman <david.shulman@microsoft.com>
2015-10-01 16:30:45 -0700

    Use JSON.NET to serialize headers

---
+++ WebServer/NameValueCollectionConverter.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Specialized;
+
+using Newtonsoft.Json;
+
+namespace WebServer
+{
+    public class NameValueCollectionConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var collection = value as NameValueCollection;
+            if (collection == null)
+                return;
+
+            writer.Formatting = Formatting.Indented;
+            writer.WriteStartObject();
+            foreach (var key in collection.AllKeys)
+            {
+                writer.WritePropertyName(key);
+                writer.WriteValue(collection.Get(key));
+            }
+            writer.WriteEndObject();
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var nameValueCollection = new NameValueCollection();
+            var key = "";
+            while (reader.Read())
+            {
+                if (reader.TokenType == JsonToken.StartObject)
+                {
+                    nameValueCollection = new NameValueCollection();
+                }
+                if (reader.TokenType == JsonToken.EndObject)
+                {
+                    return nameValueCollection;
+                }
+                if (reader.TokenType == JsonToken.PropertyName)
+                {
+                    key = reader.Value.ToString();
+                }
+                if (reader.TokenType == JsonToken.String)
+                    nameValueCollection.Add(key, reader.Value.ToString());
+            }
+            return nameValueCollection;
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            if (objectType == typeof(NameValueCollection) ||
+                objectType.BaseType == typeof(NameValueCollection) ||
+                objectType.BaseType.BaseType == typeof(NameValueCollection))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ WebServer/Test.ashx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+
+using Newtonsoft.Json;
+
+namespace WebServer
+{
+    public class EchoInformation
+    {
+        public NameValueCollection RequestHeaders { get; set; }
+        public string RequestBody { get; set; }
+    }
+
+    public class Test : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            var echo = new EchoInformation();
+            echo.RequestHeaders = context.Request.Headers;
+            echo.RequestBody = "foo";
+
+            string echoJson = JsonConvert.SerializeObject(echo, new NameValueCollectionConverter());
+
+            EchoInformation newEcho = (EchoInformation)JsonConvert.DeserializeObject(echoJson, typeof(EchoInformation), new NameValueCollectionConverter());
+            context.Response.ContentType = "text/plain"; //"application/json";
+            context.Response.Write(echoJson);
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
