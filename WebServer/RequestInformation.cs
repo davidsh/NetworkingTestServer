a71519da9d249003f52a7e2dc1087da2a1238a96
David Shulman <david.shulman@microsoft.com>
2015-10-27 09:33:09 -0700

    Add HTTP request verb and url to echo response

--- WebServer/RequestInformation.cs
+++ WebServer/RequestInformation.cs
@@ -9,6 +9,10 @@ namespace WebServer
 {
     public class RequestInformation
     {
+        public string Verb { get; private set; }
+
+        public string Url { get; private set; }
+
         public NameValueCollection Headers { get; private set; }
 
         public string BodyContent { get; private set; }
@@ -24,7 +28,8 @@ namespace WebServer
         public static RequestInformation Create(HttpRequest request)
         {
             var info = new RequestInformation();
-
+            info.Verb = request.HttpMethod;
+            info.Url = request.RawUrl;
             info.Headers = request.Headers;
 
             Stream stream = request.GetBufferedInputStream();
