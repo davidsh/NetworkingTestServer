6482c9a9df988429fcdfd24764a9a9f61b0d501f
David Shulman <david.shulman@microsoft.com>
2015-11-06 13:22:30 -0800

    Add StatusCode.ashx endpoint

---
+++ WebServer/StatusCode.ashx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Web;
+
+namespace WebServer
+{
+    public class StatusCode : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            string statusCodeString = context.Request.QueryString["statuscode"];
+            try
+            {
+                int statusCode = int.Parse(statusCodeString);
+                context.Response.StatusCode = statusCode;
+            }
+            catch (Exception)
+            {
+                context.Response.StatusCode = 500;
+                context.Response.StatusDescription = "Error parsing statuscode: " + statusCodeString;
+            }
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return true;
+            }
+        }
+    }
+}
