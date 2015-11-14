12286e8fc33dc386f0e2a01d68f05fee39aefc69
David Shulman <david.shulman@microsoft.com>
2015-11-13 20:14:08 -0800

    Fix clean close

--- WebServer/WebSocket/EchoWebSocket.ashx.cs
+++ WebServer/WebSocket/EchoWebSocket.ashx.cs
@@ -72,6 +72,8 @@ namespace WebServer
                             }
                         }
 
+                        socket.Dispose();
+
                         break;
                     }
 
