d5f7dcbfa8e4de8b8abbd4301ec19b0b3730a144
David Shulman <david.shulman@microsoft.com>
2015-12-15 14:18:52 -0800

    Cleanup

--- WebServer/RequestHelper.cs
+++ WebServer/RequestHelper.cs
@@ -8,8 +8,7 @@ namespace WebServer
     {
         public static void AddResponseCookies(HttpContext context)
         {
-            // Iterate thru the request headers.
-            // Turn all 'X-SetCookie' request headers into response cookies.
+            // Turn all 'X-SetCookie' request headers into 'Set-Cookie' response headers.
             string headerName;
             string headerValue;
             for (int i = 0; i < context.Request.Headers.Count; i++)
